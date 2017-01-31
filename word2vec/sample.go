18d3a28da95e1089748672b6ca45fa583ed9d1b0
Alex Nichol <unixpickle@gmail.com>
2017-01-30 21:26:03 -0500

    craeted helper for making word2vec samples

--- word2vec/sample.go
+++ word2vec/sample.go
@@ -12,6 +12,23 @@ type Sample struct {
 	Right []string
 }
 
+// AllSamples creates one Sample per word in a slice of
+// words.
+// The samples will all include sub-slices of the given
+// slice, so the slice should not be modified after the
+// call.
+func AllSamples(words []string) []*Sample {
+	res := make([]*Sample, len(words))
+	for i, x := range words {
+		res[i] = &Sample{
+			Left:  words[:i],
+			Word:  x,
+			Right: words[i+1:],
+		}
+	}
+	return res
+}
+
 // Trim trims the sample to have no more than the given
 // number of neighboring words on each size.
 func (s *Sample) Trim(max int) *Sample {
