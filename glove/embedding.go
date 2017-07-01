1b7b0bc1dbc3c7cdf3e627986355d7db25d0dc20
Alex Nichol <unixpickle@gmail.com>
2017-07-01 09:28:52 -0400

    added Lookup routine for nearest neighbors

--- glove/embedding.go
+++ glove/embedding.go
@@ -48,6 +48,37 @@ func (e *Embedding) EmbedID(id int) anyvec.Vector {
 	return extractRow(e.Vectors, id)
 }
 
+// Lookup finds the n closest token IDs to the given
+// vector, using the Euclidean distance.
+//
+// If n is greater than the number of IDs, then there will
+// be fewer than n results.
+func (e *Embedding) Lookup(vec anyvec.Vector, n int) []int {
+	if vec.Len() != e.Vectors.Cols {
+		panic("incorrect vector length")
+	}
+	diffs := e.Vectors.Data.Copy()
+	c := diffs.Creator()
+	diffs.Scale(c.MakeNumeric(-1))
+	anyvec.AddRepeated(diffs, vec)
+	anyvec.Pow(diffs, c.MakeNumeric(2))
+
+	distances := anyvec.SumCols(diffs, e.Vectors.Rows)
+	maxVal := anyvec.AbsMax(distances)
+	distances.Scale(c.MakeNumeric(-1))
+
+	var res []int
+	for i := 0; i < n && i < distances.Len(); i++ {
+		idx := anyvec.MaxIndex(distances)
+		res = append(res, idx)
+
+		// Make sure we don't get this ID again.
+		smallVal := c.NumOps().Mul(maxVal, c.MakeNumeric(-2))
+		distances.Slice(idx, idx+1).AddScalar(smallVal)
+	}
+	return res
+}
+
 // SerializerType returns the unique ID used to serialize
 // an Embedding with the serializer package.
 func (e *Embedding) SerializerType() string {
